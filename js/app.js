7f0c72d46b6cfa1cd1d2710e9688da2283d31789
Cédric BERNARD <cbernard@advizegroup.com>
2023-05-18 14:55:47 +0200

    init

---
+++ js/app.js
@@ -0,0 +1,129 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        personalInformations: {
+            capital:500,
+            risk: 0.5
+        },
+        stopLoss:'',
+        allPrices: '',
+        selectedPaire: '',
+        paires : [
+            {
+                name :"ETHUSDT",
+                value : ''
+            },
+            {
+                name :"SOLUSDT",
+                value : ''
+            },
+            {
+                name :"DOGEUSDT",
+                value : ''
+            },
+            {
+                name :"MATICUSDT",
+                value : ''
+            },
+            {
+                name :"ATOMUSDT",
+                value : ''
+            },
+            {
+                name :"LTCUSDT",
+                value : ''
+            },
+            {
+                name :"AVAXUSDT",
+                value : ''
+            },
+            {
+                name :"FTMUSDT",
+                value : ''
+            },
+            {
+                name :"DOTUSDT",
+                value : ''
+            },
+            {
+                name :"APEUSDT",
+                value : ''
+            },
+            {
+                name :"AXSUSDT",
+                value : ''
+            },
+            {
+                name :"BNBUSDT",
+                value : ''
+            },
+            {
+                name :"XRPUSDT",
+                value : ''
+            },
+            {
+                name :"NEARUSDT",
+                value : ''
+            },
+            {
+                name :"LINKUSDT",
+                value : ''
+            },
+            {
+                name :"SANDUSDT",
+                value : ''
+            },
+            {
+                name :"OPUSDT",
+                value : ''
+            },
+            {
+                name :"DYDXUSDT",
+                value : ''
+            }
+        ]
+    },
+    //when the page is loaded
+    created: function () {
+        this.getDatas();
+    },
+    mounted() {
+        setInterval(() => {
+            this.getDatas();
+        }, 5000);
+        if (this.paires.length > 0) {
+            this.selectedPaire = this.paires[0].name;
+            getSelectedPrice(this.selectedPaire)
+          }
+    },
+    methods: {
+        getDatas: function(){
+            axios.get('https://api.bybit.com/v2/public/tickers?')
+                .then (response  => {
+                    this.allPrices = response.data
+                }
+            );
+        },
+        calculateQty : function(){
+            console.log(this.allPrices)
+        },
+        getSelectedPrice: function(){
+            console.log(this.selectedPaire)
+            console.log(this.allPrices)
+            for (const item of this.allPrices.result) {
+                if (item.symbol.includes(this.selectedPaire)) {
+                    console.log(item.last_price)
+                  return item.last_price;
+                }
+              }
+        },
+        getAmountToRisk: function(){
+            return (this.personalInformations.capital * this.personalInformations.risk / 100)
+        },
+        quantityToInvest: function(){
+            return (
+                this.getAmountToRisk() / ( Math.max(this.getSelectedPrice(),this.stopLoss) - Math.min(this.getSelectedPrice(),this.stopLoss))
+            )
+        }
+    },
+});
